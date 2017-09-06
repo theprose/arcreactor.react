ac5d4652f6828c84ad32c6517a79a44d322b966e
Zach Stiggelbout <zstiggelbout@gmail.com>
2017-09-06 02:36:27 -0700

    favicon n stuff

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,6 @@
-'use strict';
-
-var express = require('express');
-var app = express();
-var path = require('path');
+const express = require('express');
+const app = express();
+const path = require('path');
 //const secure = require('express-force-https')
 //app.use(secure)
 
@@ -12,25 +10,25 @@ app.use('/favicon.ico', express.static(path.join(__dirname, '../public/favicon.i
 // app.use('/css', express.static(path.join(__dirname, '../public/css')))
 // app.use('/js', express.static(path.join(__dirname, '../public/js')))
 
-app.use(function (req, res, next) {
-   if (!req.secure && req.get('X-Forwarded-Proto') !== 'https' && req.hostname !== 'localhost' && req.hostname !== '127.0.0.1' && req.hostname !== 'ironman.dev') {
-      res.redirect('https://' + req.hostname + req.url);
-   } else {
-      next();
-   }
-});
+// app.use((req, res, next) => {
+//    if((!req.secure) && (req.get('X-Forwarded-Proto') !== 'https') && req.hostname !== 'localhost' && req.hostname !== '127.0.0.1' && req.hostname !== 'ironman.dev') {
+//       res.redirect('https://' + req.hostname + req.url)
+//    } else {
+//       next()
+//    }
+// })
 
-app.get('*.js', function (req, res, next) {
+app.get('*.js', (req, res, next) => {
    req.url = req.url + '.gz';
    res.set('Content-Encoding', 'gzip');
    next();
 });
 
-app.get('*', function (req, res) {
+app.get('*', (req, res) => {
    //res.send('Hello World 2!')
    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-app.listen(1337, function () {
-   console.log('Prose react app listening on port 1337.');
+app.listen(1337, () => {
+   console.log('express listening on port 1337.');
 });
